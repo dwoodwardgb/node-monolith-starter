7f16c56dd1f32777c337d1bd0b4c087f79305384
David Woodward <davidiwoodward@icloud.com>
2021-03-29 12:31:19 -0400

    Css done

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: ["./src/**/*-view.ts"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
